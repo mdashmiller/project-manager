74902cc9b1d45fd1b906fe388de9bd85b6a9a5cb
mdashmiller <m.robert.miller@gmail.com>
2018-10-26 15:43:57 -0500

    dashboard initialized

---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Notifications = () =>
+	<div>
+		<p>Notifications</p>
+	</div>
+
+export default Notifications
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ProjectList = () =>
+	<div className="section project-list">
+
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Title</span>
+				<p>Posted by Robo Robo</p>
+				<p className="grey-text">7th October, 11am</p>
+			</div>
+		</div>
+
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Title</span>
+				<p>Posted by Robo Robo</p>
+				<p className="grey-text">7th October, 11am</p>
+			</div>
+		</div>
+
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Title</span>
+				<p>Posted by Robo Robo</p>
+				<p className="grey-text">7th October, 11am</p>
+			</div>
+		</div>
+
+	</div>
+
+export default ProjectList
