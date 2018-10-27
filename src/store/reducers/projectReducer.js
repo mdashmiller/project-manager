839a4484e25df40d20d94d069b31fea98796db90
mdashmiller <m.robert.miller@gmail.com>
2018-10-27 14:22:28 -0500

    added data to init state

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+	projects: [
+		{id:'1', title: 'Get Sonic', content: 'Set up an elaborate obstacle course with a killer robot at the end' }
+		{id:'2', title: 'Sabotage Mission', content: 'Disable cryogenic systems to eleminate all shuttle crew' }
+		{id:'3', title: 'Kill All Humans', content: 'Join the League of Robots in cleansing the planet of human vermin' }
+	]
+}
 
 const projectReducer = (state = initState, action) => {
 	return state
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,7 +2,9 @@ import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers } from 'redux'
 
-export const rootReducer = combineReducers({
+const rootReducer = combineReducers({
 	auth: authReducer,
 	project: projectReducer
 })
+
+export default rootReducer
