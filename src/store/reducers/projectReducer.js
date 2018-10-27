a166b2d0a98b91e96504e43cd5f66d8aaf19c3f5
mdashmiller <m.robert.miller@gmail.com>
2018-10-27 14:42:52 -0500

    data path created

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,7 @@
 const initState = {
 	projects: [
-		{id:'1', title: 'Get Sonic', content: 'Set up an elaborate obstacle course with a killer robot at the end' }
-		{id:'2', title: 'Sabotage Mission', content: 'Disable cryogenic systems to eleminate all shuttle crew' }
+		{id:'1', title: 'Get Sonic', content: 'Set up an elaborate obstacle course with a killer robot at the end' },
+		{id:'2', title: 'Sabotage Mission', content: 'Disable cryogenic systems to eleminate all shuttle crew' },
 		{id:'3', title: 'Kill All Humans', content: 'Join the League of Robots in cleansing the planet of human vermin' }
 	]
 }
