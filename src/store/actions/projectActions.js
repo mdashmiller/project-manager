ab927bdcc8eeee3e0ea65c98b28ad804df5629b4
mdashmiller <m.robert.miller@gmail.com>
2018-11-01 12:20:14 -0500

    logged in author info added to create project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,12 +1,14 @@
 export const createProject = project => {
 	return (dispatch, getState, { getFirebase, getFirestore }) => {
-		// make asyn req to db
 		const firestore = getFirestore()
+		const profile = getState().firebase.profile
+		const authorId = getState().firebase.auth.uid
+
 		firestore.collection('projects').add({
 			...project,
-			authorFirstName: 'Doctor',
-			authorLastName: 'Robotnik',
-			authorId: 12345,
+			authorFirstName: profile.firstName,
+			authorLastName: profile.lastName,
+			authorId: authorId,
 			createdAt: new Date()
 		}).then(() => 
 			dispatch({ type: 'CREATE_PROJECT', project })
